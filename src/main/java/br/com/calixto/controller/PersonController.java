5292a6f32d0f3e94f7acfc62c6bc00f161189009
Jacks Win10 <jackson.calixto@hotmail.com>
2022-06-09 06:20:58 -0300

    Retornando Lista de Objetos

--- src/main/java/br/com/calixto/controller/PersonController.java
+++ src/main/java/br/com/calixto/controller/PersonController.java
@@ -1,17 +1,15 @@
 package br.com.calixto.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.calixto.exception.UnsuportadedMathOperationException;
-import br.com.calixto.math.SimpleMath;
 import br.com.calixto.model.Person;
-import br.com.calixto.request.converters.NumberConverter;
 import br.com.calixto.services.PersonServices;
 
 @RestController
@@ -26,5 +24,11 @@ public class PersonController {
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") String id) {
 		return services.findById(id);
+	}	
+
+	@RequestMapping(method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return services.findAll();
 	}	
 }
--- src/main/java/br/com/calixto/services/PersonServices.java
+++ src/main/java/br/com/calixto/services/PersonServices.java
@@ -1,5 +1,8 @@
 package br.com.calixto.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -11,12 +14,33 @@ public class PersonServices {
 	
 	private final AtomicLong counter = new AtomicLong();
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson();
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
 	public Person findById(String id) {
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Jackson");
 		person.setLastName("Calixto");
 		person.setAddress("Araquari - Santa Catarina - Brazil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	private Person mockPerson() {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("First Name " + person.getId());
+		person.setLastName("Last Name " + person.getId());
+		person.setAddress("Brazil " + person.getId());
 		person.setGender("Male");
 		return person;
 	}
