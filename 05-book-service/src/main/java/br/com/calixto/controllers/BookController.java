367ff7c743002aa7863099561f54c51155124461
Jacks Win10 <jackson.calixto@hotmail.com>
2022-06-20 07:19:30 -0300

    Integrating the book-service to database with JPA and Spring Data

--- 05-book-service/src/main/java/br/com/calixto/controllers/BookController.java
+++ 05-book-service/src/main/java/br/com/calixto/controllers/BookController.java
@@ -1,8 +1,6 @@
 package br.com.calixto.controllers;
 
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.calixto.model.Book;
+import br.com.calixto.repository.BookRepository;
 
 @RestController
 @RequestMapping("book-service") 
 public class BookController {
 
 	@Autowired 
-	 private Environment environment;
+	private Environment environment;
+	
+	@Autowired
+	private BookRepository repository;
 	
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(
 			@PathVariable("id") final Long id,
 			@PathVariable("currency") final String currency) {
 		
-		final var port = environment.getProperty("local.server.port");
+		var book = repository.getById(id);
+		if (book == null) throw new RuntimeException("Book not Found"); 
 		
-		return new Book(1L, "Nigel Poulton", "Docker Deep Dive", new Date(), Double.valueOf(22.7), currency, port);
+		var port = environment.getProperty("local.server.port");
+		book.setEnvironment(port);				
+		return book;
 	}
 }
--- 05-book-service/src/main/java/br/com/calixto/model/Book.java
+++ 05-book-service/src/main/java/br/com/calixto/model/Book.java
@@ -4,15 +4,38 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity(name = "book")
 public class Book implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "author", nullable = false, length = 180)
 	private String author;
+		
+	@Column(name = "launch_date", nullable = false)
 	private Date launchDate;
+	
+	@Column(nullable = false)
 	private Double price;
+	
+	@Column(nullable = false, length = 250)
 	private String title;
+	
+	@Transient
 	private String currency;
+	
+	@Transient
 	private String environment;
 	
 	public Book() {}
