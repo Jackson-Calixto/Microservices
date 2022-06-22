aa6f00ea98bbcaea3b43b38e0e6879a89732f9ae
Jacks Win10 <jackson.calixto@hotmail.com>
2022-06-22 08:59:51 -0300

    Routes with RouteLocator and Spring Cloud Gateway

--- 06-api-gateway/src/main/java/br/com/calixto/configuration/ApiGatewayConfiguration.java
+++ 06-api-gateway/src/main/java/br/com/calixto/configuration/ApiGatewayConfiguration.java
@@ -1,11 +1,6 @@
 package br.com.calixto.configuration;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +11,16 @@ public class ApiGatewayConfiguration {
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		
-		Function<PredicateSpec, Buildable<Route>> fuction =
-				p -> p.path("/get")
+		return builder.routes()
+				.route(p -> p.path("/get")
 				.filters(f -> f
 						.addRequestHeader("Hello", "World")
 						.addRequestParameter("Hello", "World"))
-					.uri("http://httpbin.org:80");
-		return builder.routes()
-				.route(fuction )
+					.uri("http://httpbin.org:80") )
+				.route(p -> p.path("/cambio-service/**")
+						.uri("lb://cambio-service"))
+				.route(p -> p.path("/book-service/**")
+						.uri("lb://book-service"))
 				.build();
 	}
 }
