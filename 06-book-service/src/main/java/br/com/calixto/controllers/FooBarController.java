2c8c8e99874a0ce8691b93717a89ca9a083f8a3b
Jacks Win10 <jackson.calixto@hotmail.com>
2022-06-23 08:17:53 -0300

    Retry with Resilience4J

--- 06-book-service/src/main/java/br/com/calixto/controllers/FooBarController.java
+++ 06-book-service/src/main/java/br/com/calixto/controllers/FooBarController.java
@@ -3,6 +3,7 @@ package br.com.calixto.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
@@ -10,6 +11,7 @@ public class FooBarController {
 
 	@GetMapping("/foo-bar")
 	public String foobar() {
+		new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
 		return "Foo-Bar!!!";
 	}
 }
